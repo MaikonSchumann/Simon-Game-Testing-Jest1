f5fd9054f5a94b6b8641eb47e4f38ba833e8d5ab
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-04-16 17:33:28 +0100

    add new game function and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,5 +1,4 @@
-const { game } = require('../game');
-
+const { game, newGame, showScore } = require('../game');
 
 beforeAll(() => {
     let fs = require('fs');
@@ -26,4 +25,27 @@ describe("game object contains correct keys", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     })
 });
+
+describe("new game", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = [1,2,3];
+        game.currentGame = [1,2,3];
+        document.getElementById('score').innerText = '42';
+        newGame();
+    });
+    test('should set game score to zero', () => {
+        expect(game.score).toEqual(0)
+    });
+    test('should clear te player moves array', () => {
+        expect(game.playerMoves).toEqual([]);
+    });
+    test("should clear te current game array", () => {
+      expect(game.currentGame).toEqual([]);
+    });
+    test('should display 0 for the element with id of score', () => {
+        expect(document.getElementById('score').innerText).toEqual(0);
+    });
+});
+
 
