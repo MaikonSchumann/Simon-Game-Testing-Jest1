4119d50b1d76446a263e3bc27b56284c27ca8824
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-04-17 07:05:38 +0100

    add lightsOn function and test

--- scripts/game.js
+++ scripts/game.js
@@ -1,26 +1,33 @@
 let game = {
-  score: 0,
   currentGame: [],
   playerMoves: [],
+  score: 0,
   choices: ["button1", "button2", "button3", "button4"],
 };
 
-function newGame(){
-    game.score = 0;
-    game.playerMoves = [];
-    game.currentGame = [];
-    showScore();
-    addTurn();
-};
-function showScore(){
-    document.getElementById("score").innerText = game.score;
-};
-    
+function newGame() {
+  game.currentGame = [];
+  game.playerMoves = [];
+  game.score = 0;
+  showScore();
+  addTurn();
+}
+
 function addTurn() {
     game.playerMoves = [];
-    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
     // showTurns();
 };
 
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+};
 
-module.exports = { game, newGame, showScore, addTurn };
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
